2fbb35852f23c9a60f6350e35a3f253ee134ff82
Onyedika Christopher <cosimon.tech@gmail.com>
2026-08-25 18:51:13 +0100

    users logic

---
+++ library/users.py
@@ -0,0 +1,34 @@
+users = {
+    "chief": {
+        "password": "chief123",
+        "position": "chief_librarian",
+        "failed_attempts": 0,
+        "locked": False
+    },
+    "member": {
+        "password": "member123",
+        "position": "member",
+        "failed_attempts": 0,
+        "locked": False
+    }
+}
+
+def authenticate(usename, password):
+
+
+    user = users.get(username)
+
+    if user is None:
+        return None
+
+    if user["locked"]:
+        return None
+
+    if user["password"] != password:
+        user["failed_attempts"] += 1
+
+        if user["failed_attempts"] >= 3:
+            user["locked"] = True
+
+            return None
+        
